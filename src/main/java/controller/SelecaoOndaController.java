9274326e78c9025fc5e0abe5da16b5f7d0db0d09
guilhermejean-cloud <guilhermejean581@gmail.com>
2026-06-22 10:12:16 -0400

    inventario fudido

--- src/main/java/controller/SelecaoOndaController.java
+++ src/main/java/controller/SelecaoOndaController.java
@@ -15,7 +15,7 @@ public class SelecaoOndaController {
     public static int ondaselecionada = 1;
 
     @FXML
-    public void acaoAbrirInventario(ActionEvent event) {
+    public void acaoabririnventario(ActionEvent event) {
         abrirtela(event, "/view/inventario.fxml");
     }
 
@@ -24,28 +24,28 @@ public class SelecaoOndaController {
             ondaselecionada = onda;
             abrirtela(event, "/view/gameplay.fxml");
         } else {
-            Alert alert = new Alert(Alert.AlertType.WARNING, "onda bloqueada! derrote a anterior.");
+            Alert alert = new Alert(Alert.AlertType.WARNING, "onda bloqueada derrote a anterior");
             alert.showAndWait();
         }
     }
 
-    @FXML public void acaoOnda1(ActionEvent e) { tentaracessaronda(1, e); }
-    @FXML public void acaoOnda2(ActionEvent e) { tentaracessaronda(2, e); }
-    @FXML public void acaoOnda3(ActionEvent e) { tentaracessaronda(3, e); }
-    @FXML public void acaoOnda4(ActionEvent e) { tentaracessaronda(4, e); }
-    @FXML public void acaoOnda5(ActionEvent e) { tentaracessaronda(5, e); }
+    @FXML public void acaoonda1(ActionEvent e) { tentaracessaronda(1, e); }
+    @FXML public void acaoonda2(ActionEvent e) { tentaracessaronda(2, e); }
+    @FXML public void acaoonda3(ActionEvent e) { tentaracessaronda(3, e); }
+    @FXML public void acaoonda4(ActionEvent e) { tentaracessaronda(4, e); }
+    @FXML public void acaoonda5(ActionEvent e) { tentaracessaronda(5, e); }
 
     private void abrirtela(ActionEvent event, String fxml) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource(fxml));
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
+            stage.setScene(new Scene(root, 800, 600));
             stage.show();
         } catch (IOException e) { }
     }
     
     @FXML
-    public void acaoVoltar(ActionEvent event) {
+    public void acaovoltar(ActionEvent event) {
         abrirtela(event, "/view/selecaopersonagem.fxml");
     }
 }
